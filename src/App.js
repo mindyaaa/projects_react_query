8a0e936b462702dbc1d79466fde0532d6188ec72
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2022-12-08 16:33:38 +0900

    useQuery 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
-  return <MainProducts />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  )
 }
